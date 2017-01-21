8f8a88dc0cb76eca3e9f0b5a40019407a6169f35
mikemajesty <mikemajesty2011@gmail.com>
2017-01-21 17:09:55 -0200

    adjust in test

--- src/core.js
+++ src/core.js
@@ -3,18 +3,10 @@
   var $P = $D.prototype;
   var $C = $D.CultureInfo;
 
-  function getMonthDays() {
+  $P.getMonthDays = function() {
     var firstDay = new Date(new Date().getFullYear(), new Date().getMonth(), 1);
     return firstDay.getDate();
     // var lastDay = new Date(this.getFullYear(), this.getMonth() + 1, 0);
-  }
-
-  $P.test = function() {
-    var firstDay = new Date(this.getFullYear(), this.getMonth(), 1);
-    return firstDay.getDate();
   };
 
-  module.exports = {
-    getMonthDays: getMonthDays
-  }
 }());
--- test/coreTest.js
+++ test/coreTest.js
@@ -3,11 +3,8 @@ var expect = chai.expect;
 var Core = require('../src/core');
 
 describe('Core', function() {
-  it('getMonthDays() should return 1', function() {
-    expect(Core.getMonthDays()).to.equal(1);
-  });
 
-  it('test() should return 1', function() {
-    expect(new Date().test()).to.equal(1);
+  it('getMonthDays() should return 1', function() {
+    expect(new Date().getMonthDays()).to.equal(1);
   });
 });
