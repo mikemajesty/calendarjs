9a8d6dd198a0da9f3da0a7bc83a4101025c0dc30
mikemajesty <mikemajesty2011@gmail.com>
2017-01-20 22:17:58 -0200

    add server js

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var favicon = require('serve-favicon');
+var app = express();
+
+app.use('/public',  express.static(__dirname + '/demo'));
+app.use('/',  express.static(__dirname + '/'));
+
+app.use(favicon(__dirname + '/public/favicon.ico'));
+
+var PORT = process.env.PORT || 3000;
+
+app.get('/', (req,res) => {
+  res.sendfile('demo/index.html');
+});
+
+app.listen(PORT, () => {
+    console.log('Server Running on '+ PORT);
+});
