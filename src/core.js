a81e6c92433372571cb518a21289a0abdc4702cd
mikemajesty <mikemajesty2011@gmail.com>
2017-01-21 16:34:54 -0200

    add first fucntion

--- src/core.js
+++ src/core.js
@@ -1,3 +1,11 @@
-(function () {
-  
-}());  
+(function() {
+  var $D = Date;
+  var $P = $D.prototype;
+  var $C = $D.CultureInfo;
+
+  $P.getMonthDays = function() {
+    var firstDay = new Date(this.getFullYear(), this.getMonth(), 1);
+    var lastDay = new Date(this.getFullYear(), this.getMonth() + 1, 0);
+  };
+
+}());
