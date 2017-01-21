9398670e352d912ecf66bcc1a57a8e7c6a6d6b9f
mikemajesty <mikemajesty2011@gmail.com>
2017-01-21 17:48:16 -0200

    add more test

--- src/core.js
+++ src/core.js
@@ -1,12 +1,39 @@
 (function() {
   var $D = Date;
   var $P = $D.prototype;
-  var $C = $D.CultureInfo;
+  var locale = 'pt-BR';
+
+  var optionsDay = { day: 'numeric' };
+  var optionsWeek = { weekday: 'long' };
 
   $P.getMonthDays = function() {
-    var firstDay = new Date(new Date().getFullYear(), new Date().getMonth(), 1);
-    return firstDay.getDate();
-    // var lastDay = new Date(this.getFullYear(), this.getMonth() + 1, 0);
+    var start = new Date(this.getFullYear(), this.getMonth(), 1);
+    var end = new Date(this.getFullYear(), this.getMonth() + 1, 0);
+
+    for (var index = start.getDate(); index <= end.getDate(); index++) {
+      start.setDate(index);
+
+    }
+    return start.getDate();
   };
+
+  $P.getWeekDays = function(tDate) {
+    var dt = new Date(tDate || this);
+    dt = new Date(dt.getFullYear(), dt.getMonth(), getDaySaturday(dt));
+    var weekList = [];
+    var tDay = parseInt(getDaySaturday(dt));
+    for (var index = 0; index < 7; index++) {
+      var day = (tDay + index);
+      var dtTemp = new Date(dt.getFullYear(), dt.getMonth(), day);
+			var week =  dtTemp.toLocaleDateString(locale, optionsWeek);
+      weekList.push(week.charAt(0).toUpperCase() + week.slice(1));
+    }
+    return weekList;
+  }
+
+  function getDaySaturday(now) {
+    now.setDate(now.getDate() + (7 + (7 - now.getDay())) % 7);
+    return now.toLocaleDateString(locale, optionsDay);
+  }
 
 }());
--- test/coreTest.js
+++ test/coreTest.js
@@ -5,6 +5,13 @@ var Core = require('../src/core');
 describe('Core', function() {
 
   it('getMonthDays() should return 1', function() {
-    expect(new Date().getMonthDays()).to.equal(1);
+    expect(new Date().getMonthDays()).to.equal(31);
   });
+
+  it('getWeekDays() should return 1', function() {
+    console.log('new Date().getWeekDays(): ', new Date().getWeekDays());
+    //expect().to.equal(1);
+  });
+
+  
 });
